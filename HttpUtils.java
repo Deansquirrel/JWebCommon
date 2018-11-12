3d09e9dcd7858134cba94da339f3c5cae1f1af0e
yuansong <yuansong6@163.com>
2018-11-12 21:19:15 +0800

    20181112

--- HttpUtils.java
+++ HttpUtils.java
@@ -16,47 +16,93 @@ public class HttpUtils {
 	
 	private static final String CHARSET = "UTF-8";
 	
-	public String httpGet(String url) {
-		StringBuffer buffer = new StringBuffer();  
-        try {  
-            URL realUrl = new URL(url);  
-            HttpURLConnection httpUrlConn = (HttpURLConnection) realUrl.openConnection();  
-   
-            httpUrlConn.setDoOutput(false);  
-            httpUrlConn.setDoInput(true);  
-            httpUrlConn.setUseCaches(false);  
-   
-            httpUrlConn.setRequestMethod("GET");  
-            httpUrlConn.connect();  
-   
-            // 将返回的输入流转换成字符串  
-            InputStream inputStream = httpUrlConn.getInputStream();  
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, CHARSET);  
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  
-   
-            String str = null;  
-            while ((str = bufferedReader.readLine()) != null) {  
-                buffer.append(str);  
-            }  
-            bufferedReader.close();  
-            inputStreamReader.close();  
-            // 释放资源  
-            inputStream.close();  
-            inputStream = null;  
-            httpUrlConn.disconnect();  
-   
-        } catch (Exception e) {  
-        	logger.error("发送GET请求出现异常！" + e.getMessage());
-	        e.printStackTrace();
+	
+	public String httpGet(String url) throws IOException {
+		StringBuffer buffer = new StringBuffer();
+
+		URL realUrl = new URL(url);
+        HttpURLConnection httpUrlConn = (HttpURLConnection) realUrl.openConnection();  
+
+        httpUrlConn.setConnectTimeout(30 * 1000);
+        httpUrlConn.setReadTimeout(30 * 1000);
+        httpUrlConn.setUseCaches(false);  
+        httpUrlConn.setRequestMethod("GET");
+        
+        httpUrlConn.setRequestProperty("Content-Type", "application/json");
+        httpUrlConn.setRequestProperty("Accept", "application/json");
+        httpUrlConn.setRequestProperty("Accept-Charset", CHARSET);
+        httpUrlConn.setRequestProperty("Accept-Language", "zh");
+        
+        httpUrlConn.connect();
+        
+        
+        
+        InputStream inputStream = null;
+        if(httpUrlConn.getResponseCode() ==HttpURLConnection.HTTP_OK  ||
+        		httpUrlConn.getResponseCode() ==HttpURLConnection.HTTP_CREATED  ||
+        		httpUrlConn.getResponseCode() ==HttpURLConnection.HTTP_ACCEPTED) {
+        	inputStream =httpUrlConn.getInputStream(); 
+        }
+        else {
+        	inputStream =httpUrlConn.getErrorStream();
+        }
+       
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, CHARSET);  
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  
+        
+        String str = null;
+        while ((str = bufferedReader.readLine()) != null) {  
+            buffer.append(str);  
         }  
-        return buffer.toString(); 
+        
+        bufferedReader.close();  
+        inputStreamReader.close();  
+        // 释放资源  
+        inputStream.close();  
+        inputStream = null;  
+        httpUrlConn.disconnect();  
+        
+        return buffer.toString();
 	}
 	
-//	private String httpGet(String url, Map<String, String> data) {
-//		return "";
+	
+//	public String httpGet(String url) {
+//		StringBuffer buffer = new StringBuffer();  
+//        try {  
+//            URL realUrl = new URL(url);  
+//            HttpURLConnection httpUrlConn = (HttpURLConnection) realUrl.openConnection();  
+//   
+//            httpUrlConn.setDoOutput(false);  
+//            httpUrlConn.setDoInput(true);  
+//            httpUrlConn.setUseCaches(false);  
+//   
+//            httpUrlConn.setRequestMethod("GET");  
+//            httpUrlConn.connect();  
+//   
+//            // 将返回的输入流转换成字符串  
+//            InputStream inputStream = httpUrlConn.getInputStream();  
+//            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, CHARSET);  
+//            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  
+//   
+//            String str = null;  
+//            while ((str = bufferedReader.readLine()) != null) {  
+//                buffer.append(str);  
+//            }  
+//            bufferedReader.close();  
+//            inputStreamReader.close();  
+//            // 释放资源  
+//            inputStream.close();  
+//            inputStream = null;  
+//            httpUrlConn.disconnect();  
+//   
+//        } catch (Exception e) {  
+//        	logger.error("发送GET请求出现异常！" + e.getMessage());
+//	        e.printStackTrace();
+//        }  
+//        return buffer.toString(); 
 //	}
 	
-	public String httpPostJson(String url, String data) {
+	public String httpPostJson(String url, String data) throws IOException {
 		OutputStreamWriter out = null;
         BufferedReader in = null;
         String result = "";
@@ -68,26 +114,26 @@ public class HttpUtils {
             conn.setDoOutput(true);
             conn.setDoInput(true);
             conn.setRequestMethod("POST");    // POST方法
-             
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setRequestProperty("Content-Type", "application/json; charset=" + CHARSET);
+            
+            conn.setRequestProperty("Content-Type", "application/json");
+	        conn.setRequestProperty("Accept", "application/json");
+	        conn.setRequestProperty("Accept-Charset", CHARSET);
+	        conn.setRequestProperty("Accept-Language", "zh");
              
             conn.connect();
              
-            out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+            out = new OutputStreamWriter(conn.getOutputStream(), CHARSET);
             out.write(data);
             out.flush();
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream(),CHARSET));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
             }
-        } catch (Exception e) {
-        	logger.error("发送 POST 请求出现异常！"+e.getMessage());
-            e.printStackTrace();
+        }catch (IOException ex) {
+        	logger.error("发送 POST 请求出现异常！"+ex.getMessage());
+        	ex.printStackTrace();
+        	throw ex;
         }
         finally{
             try{
@@ -105,4 +151,55 @@ public class HttpUtils {
         }
         return result;
 	}
+	
+	
+//	public String httpPostJson(String url, String data) {
+//		OutputStreamWriter out = null;
+//        BufferedReader in = null;
+//        String result = "";
+//        try {
+//            URL realUrl = new URL(url);
+//            HttpURLConnection conn = null;
+//            conn = (HttpURLConnection) realUrl.openConnection();
+//
+//            conn.setDoOutput(true);
+//            conn.setDoInput(true);
+//            conn.setRequestMethod("POST");    // POST方法
+//             
+//            conn.setRequestProperty("accept", "*/*");
+//            conn.setRequestProperty("connection", "Keep-Alive");
+//            conn.setRequestProperty("user-agent",
+//                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+//            conn.setRequestProperty("Content-Type", "application/json; charset=" + CHARSET);
+//             
+//            conn.connect();
+//             
+//            out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+//            out.write(data);
+//            out.flush();
+//            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                result += line;
+//            }
+//        } catch (Exception e) {
+//        	logger.error("发送 POST 请求出现异常！"+e.getMessage());
+//            e.printStackTrace();
+//        }
+//        finally{
+//            try{
+//                if(out!=null){
+//                    out.close();
+//                }
+//                if(in!=null){
+//                    in.close();
+//                }
+//            }
+//            catch(IOException ex){
+//            	logger.error(ex.getMessage());
+//                ex.printStackTrace();
+//            }
+//        }
+//        return result;
+//	}
 }
